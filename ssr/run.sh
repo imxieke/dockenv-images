d800e94b5d0508c2c0d5f14edb455ebb044db2b7
imxieke <imxieke@qq.com>
2017-03-28 19:28:47 +0800

    update

--- ssr/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-exec /usr/bin/ssserver -p ${PORT} -k ${PASSWD}  -O auth_sha1_v4 -o tls1.2_ticket_auth --user nobody -d start
-echo "Passwd:"${PASSWD}
-echo "port:"${PORT}
-echo "enjoy it!"
