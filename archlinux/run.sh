1eb9e5ccf50e692ff934d2d2a4e51f773b626e61
imxieke <imxieke@qq.com>
2017-05-18 12:32:48 +0800

    update Archlinux

--- archlinux/run.sh
+++ archlinux/run.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-PASS=${ROOT_PASS}
 USER=${USERNAME}
-useradd -d /home/$USERNAME -m $USERNAME -s /bin/bash
-echo "$USERNAME:$PASS" |chpasswd
+PASS=${PASSWORD}
+useradd -d /home/$USER -m $USER -s /bin/bash
+echo "$USER:$PASS" |chpasswd
 echo "root:$PASS" | chpasswd
-echo "$USERNAME ALL=NOPASSWD: ALL" >>/etc/sudoers
+echo "$USER ALL=NOPASSWD: ALL" >>/etc/sudoers
 echo "======================================================================"
 echo "You can now connect to this container via SSH using:					"
-echo "    ssh $USERNAME@host -p port 										"
-echo "Enter the $USERNAME password '$PASS' when prompted					"
+echo "    ssh $USER@host -p port 										"
+echo "Enter the $USER password '$PASS' when prompted					"
 echo "Please remember to change the above password as soon as possible!		"
 echo "======================================================================"
 
